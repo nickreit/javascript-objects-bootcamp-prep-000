f6a470e8c1a39d8ac0ea7e6fe9bffa60381ee66b
nickreit@icloud.com <nickreit@icloud.com>
2018-09-12 20:30:50 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
 var playlist = new Object({artistName:"song"})
-function updatePlaylist(object, artistName, song) {
+function updatePlaylist(playlist, artistName, songTitle) {
 playlist.artistName = "song"
 }
